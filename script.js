91c5af447dcca7d307bf5cde61e7028606e36113
Zile <zivanovicmilos86@gmail.com>
2022-09-30 00:21:09 +0200

    commit all

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const popUp = document.querySelector(".popup_content");
+const popUpContainer = document.querySelector("#popup");
+
+popUpContainer.addEventListener("click", function (e) {
+  alert("JUST FUCKING CLOSE IT!!!");
+});
